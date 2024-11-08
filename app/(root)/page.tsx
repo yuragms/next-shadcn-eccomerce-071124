41a97567a528cbf8606bf5fe7c1a5574d2cb4798
yuragms <yura.gms@gmail.com>
2024-11-08 12:43:47 +0200

    5-list-products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
+import ProductList from '@/components/shared/product/product-list';
 import { Button } from '@/components/ui/button';
+import sampleData from '@/lib/sample-data';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Button</Button>
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList data={sampleData.products} />
     </div>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
