37504f09f3827b0efdfecc481ead092bdd903a4b
yuragms <yura.gms@gmail.com>
2024-11-17 21:21:22 +0200

    23-mark-orders-to-paid-and-delivered

--- app/(root)/order/[id]/order-details-form.tsx
+++ app/(root)/order/[id]/order-details-form.tsx
@@ -23,13 +23,19 @@ import Link from 'next/link';
 import {
   approvePayPalOrder,
   createPayPalOrder,
+  deliverOrder,
+  updateOrderToPaidByCOD,
 } from '@/lib/actions/order.actions';
+import { Button } from '@/components/ui/button';
+import { useTransition } from 'react';
 export default function OrderDetailsForm({
   order,
   paypalClientId,
+  isAdmin,
 }: {
   order: Order;
   paypalClientId: string;
+  isAdmin: boolean;
 }) {
   const {
     shippingAddress,
@@ -72,6 +78,49 @@ export default function OrderDetailsForm({
       description: res.message,
       variant: res.success ? 'default' : 'destructive',
     });
+  };
+
+  const MarkAsPaidButton = () => {
+    const [isPending, startTransition] = useTransition();
+    const { toast } = useToast();
+    return (
+      <Button
+        type="button"
+        disabled={isPending}
+        onClick={() =>
+          startTransition(async () => {
+            const res = await updateOrderToPaidByCOD(order.id);
+            toast({
+              variant: res.success ? 'default' : 'destructive',
+              description: res.message,
+            });
+          })
+        }
+      >
+        {isPending ? 'processing...' : 'Mark As Paid'}
+      </Button>
+    );
+  };
+  const MarkAsDeliveredButton = () => {
+    const [isPending, startTransition] = useTransition();
+    const { toast } = useToast();
+    return (
+      <Button
+        type="button"
+        disabled={isPending}
+        onClick={() =>
+          startTransition(async () => {
+            const res = await deliverOrder(order.id);
+            toast({
+              variant: res.success ? 'default' : 'destructive',
+              description: res.message,
+            });
+          })
+        }
+      >
+        {isPending ? 'processing...' : 'Mark As Delivered'}
+      </Button>
+    );
   };
   return (
     <>
@@ -180,6 +229,10 @@ export default function OrderDetailsForm({
                   </PayPalScriptProvider>
                 </div>
               )}
+              {isAdmin && !isPaid && paymentMethod === 'CashOnDelivery' && (
+                <MarkAsPaidButton />
+              )}
+              {isAdmin && isPaid && !isDelivered && <MarkAsDeliveredButton />}
             </CardContent>
           </Card>
         </div>
--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getOrderById } from '@/lib/actions/order.actions';
 import { APP_NAME } from '@/lib/constants';
 import { notFound } from 'next/navigation';
 import OrderDetailsForm from './order-details-form';
+import { auth } from '@/auth';
 export const metadata = {
   title: `Order Details - ${APP_NAME}`,
 };
@@ -12,6 +13,7 @@ const OrderDetailsPage = async ({
     id: string;
   };
 }) => {
+  const session = await auth();
   const order = await getOrderById(id);
   if (!order) notFound();
   order.user;
@@ -19,6 +21,7 @@ const OrderDetailsPage = async ({
     <OrderDetailsForm
       order={order}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+      isAdmin={session?.user.role === 'admin' || false}
     />
   );
 };
